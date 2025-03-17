4733f8d36b0acb7d1b18d6919380f2681c4a698c
ClemAMolette <clementinegaga79@outlook.fr>
2025-03-17 15:17:50 +0100

    récupération des masques de l'image uploadée

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
 import streamlit as st
-import base64
+from PIL import Image
+from src.get_masks import get_masks
+import os
 
 st.set_page_config(page_title="LivrIA", page_icon="📚")
 with open("src/style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 
+output_folder = "uploaded_images/"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 primary_color = 'rgb(89, 24, 178)'
 
@@ -28,4 +33,14 @@ with cent:
     if photo is not None:
         st.markdown("La machine travaile ...")
         st.image('src/cat-cute.gif')
-    
+
+        # on convertit l'image uploadée par l'utilisateur dans le format compris par notre modèle
+        img = Image.open(photo)
+        img_path = os.path.join(output_folder, photo.name)
+        img.save(img_path)
+
+        # récupération de tous les masques de livre de l'image
+        masks = get_masks(img_path)
+        for mask in masks:
+            st.image(mask)
+
