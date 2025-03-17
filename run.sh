06754121af2202150f44324383a6cfd1d1e8a70d
ClemAMolette <clementinegaga79@outlook.fr>
2025-03-17 14:15:07 +0100

    changement theme de couleurs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd)
-streamlit run src/app.py
+streamlit run src/app.py --theme.primaryColor="rgb(203, 166, 247)" --theme.backgroundColor="rgb(61, 15, 125)" --theme.textColor="rgb(205, 214, 244)" --theme.base="dark"
